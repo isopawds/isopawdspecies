39917c33c024f51658dc2b68a358f0f946710a8e
isopawds <subwoofieworks@gmail.com>
2026-01-10 19:30:07 -0500

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -15,9 +15,9 @@ let charadex = {};
 /* Any preview links will still show Charadex's information
 /* ==================================================================== */
 charadex.site = {
-  title: "Charadex",
-  url: "https://charadex-team.github.io/charadex-v1.0/",
-  description: `A tool for organizing small ARPGs and species.`
+  title: "ISOPAWDS",
+  url: "https://isopawdsspecies.netlify.app/",
+  description: `A closed-species ARPG with isopod-like dragons!`
 }
 
 /* ==================================================================== */
@@ -26,7 +26,7 @@ charadex.site = {
 /* ==================================================================== */
 charadex.sheet = {
 
-  id: "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak",
+  id: "1ot0iF6FnL39RhZlz3PJYW6n2Wl2cjsXzMCQkX_td6WY",
 
   pages: {
     masterlist:    "masterlist",
@@ -44,8 +44,8 @@ charadex.sheet = {
 
     designTypes: ['All', 'Official Design', 'Guest Design', 'MYO Slot', 'MYO Design'],
     statuses: ['All', 'Resell', 'Trade', 'Gift', 'Voided', 'For Sale', 'Purchased'],
-    rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Very Rare', 'Legendary'],
-    species: ['All', 'Dog', 'Cat', 'Bunny'],
+    rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Legendary', 'Mythical', 'Moderator'],
+    species: ['All', 'Isopawd'],
     itemTypes: ['All', 'Currency', 'MYO Slot', 'Pet', 'Trait', 'Misc'],
     traitTypes: ['All', 'Ears', 'Eyes', 'Body', 'Limbs', 'Tails', 'Misc', 'Mutations']
 
